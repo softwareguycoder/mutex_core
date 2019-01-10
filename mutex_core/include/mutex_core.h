420e177e71d04330a316c741950f2a2032bb162b
softwareguycoder <softwareguycoder@gmail.com>
2019-01-10 09:15:24 -0500

    10 Jan 2019 Update to minor Eclipse settings file

--- mutex_core/include/mutex_core.h
+++ mutex_core/include/mutex_core.h
@@ -40,20 +40,21 @@ HMUTEX CreateMutex();
 void DestroyMutex(HMUTEX hMutex);
 
 /**
- * \brief Locks the mutex with the handle specified.  Does nothing if the handle
+ * @brief Locks the mutex with the handle specified.  Does nothing if the handle
  * is INVALID_HANDLE_VALUE.
- *
  * @param hMutex Handle of type HMUTEX (see stdafx.h) that refers to the mutex you want
  * to obtain a lock for.
+ * @returns TRUE (nonzero) if a lock was successfully obtained; FALSE (zero) if a problem occurred.
  */
-void LockMutex(HMUTEX hMutex);
+BOOL LockMutex(HMUTEX hMutex);
 
 /**
- * \brief Releases any existing locks on the mutex referred to by the handle specified. Does
+ * @brief Releases any existing locks on the mutex referred to by the handle specified. Does
  * nothing if the handle is INVALID_HANDLE_VALUE.
  * @param hMutex Mutex handle of type HMUTEX (see stdafx.h) that refers to the mutex you want
  * to release the lock on.
+ * @returns TRUE if we successfully released the lock; FALSE if a problem occurred.
  */
-void UnlockMutex(HMUTEX hMutex);
+BOOL UnlockMutex(HMUTEX hMutex);
 
 #endif /* __MUTEX_CORE_H__ */
